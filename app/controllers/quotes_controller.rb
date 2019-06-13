f5f67429e46756604863324209f2cfeb64eec8e1
Raya Anderson <rayazand@gmail.com>
2019-06-13 07:26:35 +0000

    colors and pictures

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,9 @@ def index
   @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
+ 
+
+ 
   def create
      @quote = Quote.create(quote_params)
   if @quote.invalid?
@@ -13,6 +13,11 @@ def index
   end
      redirect_to root_path
   end
+
+  def about
+  end
+
+
   private
 
   def quote_params
